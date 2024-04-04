d37d4a9dcafe9f06a41e684a021d3bbd3bbb375e
shivam <Shivam>
2024-04-05 00:55:09 +0530

    UI has been Changed

---
+++ app/src/main/java/com/motiv/motiv8/model/HomePage.java
@@ -0,0 +1,16 @@
+package com.motiv.motiv8.model;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.motiv.motiv8.R;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
